2d012f1dfb7a163aaf08287ab003e99abbf44b56
KimJeeHun1 <iopuioyui@naver.com>
2026-03-27 09:51:46 +0900

    Update 3-2.c

--- code/3-2/3-2.c
+++ code/3-2/3-2.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
+ /* unsigned char* addr = (unsigned char*)&x;
 
+    printf("%x\n", *addr);
+    printf("%x\n", *(addr + 1));
+    printf("%x\n", *(addr + 2));
+    printf("%x\n", *(addr + 3));
+    */
 void swap_endian(int *x){
-  
+  unsigned char* addr = (unsigned char*)x;
+  unsigned char temp;
+  temp = *addr;
+  *addr = *(addr +3);
+  *(addr + 3) = temp;
+   temp = *(addr +1);
+   *(addr+1)=*(addr+2);
+   *(addr+2)=temp;
 }
 
 int main(){
